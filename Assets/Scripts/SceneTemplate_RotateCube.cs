5b6f018243f6544dbca0ce0d84cc85fa931cd2fd
Şeyma Altıparmak <seyma.altiparmak@ogr.sakarya.edu.tr>
2024-02-07 22:36:26 +0300

    Day 1 end

---
+++ Assets/Scripts/SceneTemplate_RotateCube.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneTemplate_RotateCube : MonoBehaviour
+{
+    [SerializeField] GameObject myCube;
+    [SerializeField] float rotationSpeed;
+    private Vector3 turnRotation;
+    private void Awake()
+    {
+        turnRotation = Vector3.forward;
+        myCube = FindAnyObjectByType<CubeObject>().gameObject;
+    }
+
+    private void Update()
+    {
+        myCube.transform.Rotate(turnRotation * rotationSpeed * Time.deltaTime);
+    }
+}
